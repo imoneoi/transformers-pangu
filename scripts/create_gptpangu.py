b22bcb75a908920170f0cd8035d8fb17adb1da75
one <imone@tuta.io>
2021-12-13 14:23:50 +0800

    test generation pipeline

--- scripts/create_gptpangu.py
+++ scripts/create_gptpangu.py
@@ -5,4 +5,5 @@ if __name__ == "__main__":
     tokenizer = transformers.AutoTokenizer.from_pretrained("../data/transformers", trust_remote_code=True)
     model = transformers.AutoModelForCausalLM.from_pretrained("../data/transformers", trust_remote_code=True)
 
-    print(model)
+    text_generator = transformers.TextGenerationPipeline(model, tokenizer)
+    print(text_generator("中国和美国和日本和法国和加拿大和澳大利亚的首都分别是哪里？", max_length=50))
